c831648fdb88138f44059525773d5ea6a5fbcd4c
Ринат Хисамов <khisamov99@gmail.com>
2020-03-26 00:36:21 +0400

    Laba3.1

---
+++ Secure/SecureShopDatabaseImplement/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using SecureLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SecureShopDatabaseImplement.Models
+{
+    /// Заказ
+    /// </summary>
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        public decimal Sum { get; set; }
+        [Required]
+        public OrderStatus Status { get; set; }
+        [Required]
+        public DateTime DateCreate { get; set; }
+        public DateTime? DateImplement { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
