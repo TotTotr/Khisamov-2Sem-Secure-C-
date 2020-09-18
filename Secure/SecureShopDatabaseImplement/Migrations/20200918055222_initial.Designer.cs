13c83565f77de92ed90292646806a78b9656cd14
Ринат Хисамов <khisamov99@gmail.com>
2020-09-18 10:03:37 +0400

    laba5final

--- Secure/SecureShopDatabaseImplement/Migrations/20200918055222_initial.Designer.cs
+++ Secure/SecureShopDatabaseImplement/Migrations/20200918055222_initial.Designer.cs
@@ -10,7 +10,7 @@ using SecureShopDatabaseImplement;
 namespace SecureShopDatabaseImplement.Migrations
 {
     [DbContext(typeof(SecureShopDatabase))]
-    [Migration("20200918060325_initial")]
+    [Migration("20200918055222_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
